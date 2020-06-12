c43cd0171e425c6629af45b9a311c3c2596fd2bd
Lisa <lisa870219@gmail.com>
2020-06-12 12:09:13 +0800

    Hani_1

---
+++ keywords.js
@@ -0,0 +1,260 @@
+var formatDateIntoYear = d3.time.format("%Y");
+var formatDate = d3.time.format("%b %Y");
+//var parseDate = d3.timeParse("%m/%y");
+
+var startDate = new Date("2010-01-01"),
+    endDate = new Date("2019-12-31");
+
+var topic = ["政治", "社會", "影劇", "醫藥", "科技"]
+
+var margin = {
+        top: 50,
+        right: 50,
+        bottom: 50,
+        left: 50
+    },
+    width = 960 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom;
+
+var svg = d3.select("#vis")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom);
+
+
+////////// plot //////////
+
+var dataset;
+var color = d3.scale.category20();
+
+var pack = d3.layout.pack()
+    .size([width, height])
+    .padding(3)
+    .radius(30);
+
+var plot = svg.append("g")
+    .attr("class", "plot")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("final_issue_2010_2019.json", function(data) {
+    console.log("HELLO");
+    dataset = data;
+    drawPlot("2010/1", dataset["2010/1"]);
+    playButton
+        .on("click", function() {
+            var button = d3.select(this);
+            if (button.text() == "Pause") {
+                moving = false;
+                clearInterval(timer);
+                // timer = 0;
+                button.text("Play");
+            } else {
+                moving = true;
+                timer = setInterval(step, 100);
+                button.text("Pause");
+            }
+            console.log("Slider moving: " + moving);
+        })
+})
+
+function step() {
+    update(x.invert(currentValue));
+    currentValue = currentValue + (targetValue / 151);
+    if (currentValue > targetValue) {
+        moving = false;
+        currentValue = 0;
+        clearInterval(timer);
+        // timer = 0;
+        playButton.text("Play");
+        console.log("Slider moving: " + moving);
+    }
+}
+
+function drawPlot(time, data) {
+    console.log(time, Object.keys(data));
+
+    var political = data[topic[0]];
+    var social = data[topic[1]];
+    var entertainment = data[topic[2]];
+    var medicine = data[topic[3]];
+    var technique = data[topic[4]];
+
+    //console.log(Object.keys(political)[0], Object.values(political)[0]);
+    // 建立新的json格式
+    var dataStructure = '{"year": "' + time + '", "children": [';
+    for (var i = 0; i < topic.length; i++) {
+        dataStructure += '{ "topic": "' + topic[i] + '", "children":[';
+        len = Object.keys(data[topic[i]]).length
+        for (var j = 0; j < len; j++) {
+            words = JSON.stringify(Object.values(data[topic[i]])[j])
+            value = len - j;
+            dataStructure += '{ "name":"' + Object.keys(data[topic[i]])[j] + '","value":' + value.toString() + ',"key":' + words + '}';
+            if (j < Object.keys(data[topic[i]]).length - 1) dataStructure += ',';
+        }
+        dataStructure += ']}';
+        if (i < topic.length - 1) dataStructure += ',';
+    }
+    dataStructure += ']}'
+    console.log(dataStructure)
+    dataStructure = JSON.parse(dataStructure)
+    console.log(dataStructure);
+
+    var nodes = pack.nodes(dataStructure);
+
+    var links = pack.links(nodes);
+
+    nodes = nodes.filter(function(it) { return it.parent; });
+
+    circle = plot.selectAll("circle").data(nodes);
+    circle.exit().remove();
+    circle.enter()
+        .append("circle")
+        // .attr("fill", "rgb(31, 119, 180)")
+        .attr("fill", function(d) { return color(d.topic); })
+        .attr("fill-opacity", "0.8")
+        .attr("cx", function(d) {
+            return d.x;
+        })
+        .attr("cy", function(d) {
+            return d.y;
+        })
+        .attr("r", function(d) {
+            console.log(d.name + "," + d.value);
+            if (d.depth == 2) return d.value * 3 + 20;
+            else return d.r;
+        });
+    circle.on("mouseover", function(d, i) {
+            d3.select(this)
+                .attr("fill", function(d) { return color(d.name); })
+        })
+        .on("mouseout", function(d, i) {
+            d3.select(this)
+                .attr("fill", function(d) { return color(d.topic); });
+        });
+
+
+    text = plot.selectAll("text").data(nodes);
+    text.exit().remove();
+    text.enter()
+        .append("text")
+        .attr("font-size", "10px")
+        .attr("fill", "white")
+        .attr("fill-opacity", function(d) {
+            if (d.depth == 2)
+                return "0.9";
+            else
+                return "0";
+        })
+        .attr("x", function(d) {
+            return d.x;
+        })
+        .attr("y", function(d) {
+            return d.y;
+        })
+        .attr("dx", -12)
+        .attr("dy", 1)
+        .text(function(d) {
+            return d.name;
+        });
+
+}
+var pretime = "";
+
+function update(h) {
+    var year = h.getFullYear();
+    var month = (parseInt(h.getMonth()) + 1);
+    switch (month) {
+        case 2:
+        case 3:
+            month = 1;
+            break;
+        case 5:
+        case 6:
+            month = 4;
+            break;
+        case 8:
+        case 9:
+            month = 7;
+            break;
+        case 11:
+        case 12:
+            month = 10;
+            break;
+    }
+    var time = year + "/" + month;
+    // update position and text of label according to slider scale
+    handle.attr("cx", x(h));
+    label.attr("x", x(h))
+        .text(formatDate(h));
+
+    // filter data set and redraw plot
+    if (pretime !== time) {
+        var newData = dataset[time]
+        drawPlot(time, newData);
+    }
+    pretime = time;
+}
+////////// slider //////////
+
+var moving = false;
+var currentValue = 0;
+var targetValue = width;
+
+var playButton = d3.select("#play-button");
+
+var x = d3.time.scale()
+    .domain([startDate, endDate])
+    .range([0, targetValue])
+    .clamp(true);
+
+var slider = svg.append("g")
+    .attr("class", "slider")
+    .attr("transform", "translate(" + margin.left + "," + height + ")");
+
+slider.append("line")
+    .attr("class", "track")
+    .attr("x1", x.range()[0])
+    .attr("x2", x.range()[1])
+    .select(function() {
+        return this.parentNode.appendChild(this.cloneNode(true));
+    })
+    .attr("class", "track-inset")
+    .select(function() {
+        return this.parentNode.appendChild(this.cloneNode(true));
+    })
+    .attr("class", "track-overlay")
+    .call(d3.behavior.drag()
+        .on("dragend", function() {
+            slider.interrupt();
+        })
+        .on("drag", function() {
+            currentValue = d3.event.x;
+            update(x.invert(currentValue));
+        })
+    );
+
+slider.insert("g", ".track-overlay")
+    .attr("class", "ticks")
+    .attr("transform", "translate(0," + 18 + ")")
+    .selectAll("text")
+    .data(x.ticks(10))
+    .enter()
+    .append("text")
+    .attr("x", x)
+    .attr("y", 10)
+    .attr("text-anchor", "middle")
+    .text(function(d) {
+        return formatDateIntoYear(d);
+    });
+
+var handle = slider.insert("circle", ".track-overlay")
+    .attr("class", "handle")
+    .attr("r", 9);
+
+var label = slider.append("text")
+    .attr("class", "label")
+    .attr("text-anchor", "middle")
+    .text(formatDate(startDate))
+    .attr("transform", "translate(0," + (-25) + ")")
